ad904b145269d7b907d8fc00e0c151f7e1dbe486
Drew <DGriffin@appacademy.io>
2022-12-04 23:07:20 -0600

    Remove 2nd homepage button

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -5,18 +5,12 @@ import { Redirect } from 'react-router-dom';
 
 function Home() {
   const dispatch = useDispatch();
-  //top buttons
+
   let cryptos = useSelector(state => state.session.cryptos);
   let stocks = useSelector(state => state.session.stocks);
-
-  //bottom buttons
-  let cryptosBottom = useSelector(state => state.session.cryptos);
-  let stocksBottom = useSelector(state => state.session.stocks);
   
   let cryptoNameArr = []
   let stockNameArr = []
-  let cryptoNameArrBottom = []
-  let stockNameArrBottom = []
 
   if (cryptos) {
       cryptoNameArr = cryptos.assets;
@@ -26,14 +20,6 @@ function Home() {
       stockNameArr = stocks.stockSymbols;
   }  
   
-  if (cryptosBottom) {
-      cryptoNameArrBottom = cryptosBottom.assets;
-  }
-    
-  if (stocksBottom) {
-      stockNameArrBottom = stocksBottom.stockSymbols;
-  }  
-
   const setCryptos = (e) => {
     e.preventDefault();
       dispatch(sessionActions.getCryptoNames()); 
@@ -45,25 +31,6 @@ function Home() {
       dispatch(sessionActions.getStockNames()); 
       cryptos.assets = [];
   }  
-  const setCryptosBottom = (e) => {
-    e.preventDefault();
-      dispatch(sessionActions.getCryptoNames()); 
-      stocksBottom.stockSymbols = [];
-  }
-    
-  const setStocksBottom = (e) => {
-    e.preventDefault();
-      dispatch(sessionActions.getStockNames()); 
-      cryptosBottom.assets = [];
-  }  
-    /*
-    if (cryptos === null) {
-      dispatch(sessionActions.getCryptoNames())
-      //return <Redirect to="/" />
-  } else {
-    nameArr = cryptos.assets
-  }
-  */
     
   return (
     <div>
@@ -83,29 +50,6 @@ function Home() {
         <br></br>
           {stockNameArr && 
               stockNameArr.map((name => {
-                return <button>{name}</button> 
-                }))
-          }
-      </div>
-      <div>
-        Middle section
-      </div>
-           <div>
-        <p>Select An Asset Class</p>
-        <button onClick={setStocksBottom}>Stocks</button>
-        <button onClick={setCryptosBottom}>Cryptos</button>
-        <br></br>
-        <br></br>
-          {cryptoNameArrBottom && 
-              cryptoNameArrBottom.map((name => {
-                name = name.split('USD')[0]
-                return <button>{name}</button> 
-                }))
-          }
-        <br></br>
-        <br></br>
-          {stockNameArrBottom && 
-              stockNameArrBottom.map((name => {
                 return <button>{name}</button> 
                 }))
           }
